f4d0952ef00002cae2f8735ab3eaf94258152cff
Nicola <nqola.ibrahim@lit-co.net>
2023-03-06 09:43:55 +0200

    Added entropy method for weights tunning

--- ranking_algorithm/core/core.py
+++ ranking_algorithm/core/core.py
@@ -1,78 +1,91 @@
-def cal_std(combs_time_span: list[int], target: int) -> float:
-    """Calculate the std for the spaces' combination time span
+import math
+
+import pandas as pd
+
+from . import settings
+
+
+def create_decision_matrix(data: pd.DataFrame = None) -> pd.DataFrame:
+    """Create decision matrix for ranking algorithm
 
     Args:
-        combs_time (list[int]): available time list for the spaces combinations
-        target (int): the target time value to be obtained
+        data (pd.DataFrame, optional): spaces data. Defaults to None.
 
     Returns:
-        list[float]: list of z-scores for the spaces' combination
+        pd.DataFrame: the created decision matrix
     """
-    var = sum(pow(x - target, 2) for x in combs_time_span) / len(combs_time_span)
-    std = math.sqrt(var)
+    # Read spaces data file
+    processed_combinations_data = (
+        pd.read_json(settings.PROC_SPACES_DATA_PATH)
+        if data is None
+        else pd.DataFrame(data)
+    )
 
-    return std
+    matrix = pd.DataFrame(
+        columns=["total_time_range", "num_spaces", "num_cancellable_spaces"],
+        index=processed_combinations_data.columns,
+    )
+
+    # Loop to take only value of specific attribute
+    for comb, details in processed_combinations_data.items():
+        for det, value in details.items():
+            if det in ["total_time_range", "num_spaces", "num_cancellable_spaces"]:
+                matrix[det][comb] = value
+
+    decision_matrix = pd.DataFrame(matrix)
+
+    # decision_matrix.to_pickle(settings.DECISION_MATRIX_PATH)
+    return decision_matrix
 
 
-def cal_zscores(combs_time_span: list[int], target: int) -> list[float]:
-    """Calculate the z-scores for the spaces' combination
+def entropy_weights_method(
+    goal_time: int, decision_matrix: pd.DataFrame = None
+) -> pd.DataFrame:
+    """Tunning the criteria weights using entropy method for ranking system
 
     Args:
-        combs_time (list[int]): available time list for the spaces combinations
-        target (int): the target time value to be obtained
+        goal_time (int): the desired time span
+        decision_matrix (pd.DataFrame, optional): spaces combinations details. Defaults to None.
 
     Returns:
-        list[float]: list of z-scores for the spaces' combination
+        pd.DataFrame: weights dataframe
     """
-    std = cal_std(combs_time_span)
+    dmatrix = (
+        pd.read_pickle(settings.DECISION_MATRIX_PATH)
+        if decision_matrix is None
+        else decision_matrix
+    )
 
-    z_scores = [(time_span - target) / std for time_span in combs_time_span]
+    # Substitute total_time_range by distance column
+    dmatrix["distance"] = (dmatrix["total_time_range"] - goal_time).abs()
+    dmatrix.drop(columns=["total_time_range"], inplace=True)
 
-    return z_scores
+    # Start the algorithm
+    rows, cols = dmatrix.shape
+    k = 1.0 / math.log(rows)
 
+    lnf = [[None] * cols for i in range(rows)]
 
-def get_score(z_score: float, num_cancellable_spaces: int, num_spaces: int) -> float:
-    """Get the score for a specific spaces combination
+    for i in range(0, rows):
+        for j in range(0, cols):
+            if dmatrix.iloc[i][j] == 0:
+                lnfij = 0.0
+            else:
+                p = dmatrix.iloc[i][j] / dmatrix.iloc[:, j].sum()
+                lnfij = math.log(p) * p * (-k)
+            lnf[i][j] = lnfij
+    lnf = pd.DataFrame(
+        lnf, index=decision_matrix.index, columns=decision_matrix.columns
+    )
 
-    Args:
-        z_score (float): combination z_score value
-        num_cancellable_spaces (int): combination num of cancellable spaces
-        num_spaces (int): combination num of spaces
+    e = 1 - lnf.sum(axis=0)
+    weights = [[None] * 1 for i in range(cols)]
+    for j in range(0, cols):
+        weightsj = e[j] / sum(e)
+        weights[j] = weightsj
 
-    Returns:
-        float: score value
-    """
-    if z_score > 0:
-        score = 1 / (
-            (0.6 * num_cancellable_spaces) + (0.3 * num_spaces) + (0.1 * z_score)
-        )
-
-    else:
-        score = (0.1 * z_score) / ((0.6 * num_cancellable_spaces) + (0.3 * num_spaces))
-
-    return round(score, 3)
-
-
-def cal_scores(
-    z_scores: list[float], cancellable_spaces: list[int], spaces: list[int]
-) -> list[float]:
-    """Calculate the scores list for the spaces combinations
-
-    Args:
-        z_scores (list[float]): list of z_scores for the spaces' combination
-        cancellable_spaces (list[int]): num of cancellable spaces for each the spaces' combination
-        spaces (list[int]): num of spaces spaces for each the spaces' combination
-
-    Returns:
-        list[float]: _description_
-    """
-
-    scores = []
-
-    for z_score, num_cancellable_spaces, num_spaces in zip(
-        z_scores, cancellable_spaces, spaces
-    ):
-
-        scores.append(get_score(z_score, num_cancellable_spaces, num_spaces))
-
-    return scores
+    weights = pd.DataFrame(weights)
+    weights = weights.round(5)
+    weights.index = dmatrix.columns
+    weights.columns = ["weight"]
+    return weights
