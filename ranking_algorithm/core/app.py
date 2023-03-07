4e7290e2ff20a192cc565f38e633e8c312b1d9c6
Nicola <nqola.ibrahim@lit-co.net>
2023-03-07 13:27:10 +0200

    Refactored

--- ranking_algorithm/core/app.py
+++ ranking_algorithm/core/app.py
@@ -1,11 +1,11 @@
-from . import topsis, tunning, utils
+from . import rank, utils
 
 
 def run():
     goal_time = 8
     encoded_time_range = utils.get_encoded_spaces_time_range()
     space_combinations = utils.get_encoded_spaces_combinations(encoded_time_range)
-    decision_matrix = tunning.create_decision_matrix(
+    decision_matrix = rank.create_decision_matrix(
         goal_time=goal_time, data=space_combinations
     )
     # weights = tunning.entropy_weights_method(
@@ -20,7 +20,7 @@ def run():
     # weights = [72.7, 20.0, 7.3]  # CR = 1%
     weights = [69.2, 23.1, 7.7]  # CR = 0%
     # weights = [74.7, 13.4, 11.9]  # CR = 0% (num_spaces == distance)
-    t = topsis.Topsis(decision_matrix, weights, criteria=[False, False, False])
+    t = rank.Topsis(decision_matrix, weights, criteria=[False, False, False])
 
     t.get_rank()
 
